cb04c95335f7307bf79a04775639926a0777a690
Jakub Červený <cervenyjakub98@gmail.com>
2021-02-28 15:27:40 +0100

    Version 1.3.1 Default admin state is now false

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data): User
     {
-        $admin = true;
+        $admin = false;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
